c796f0310400407eac4d625b10a4801e57fc5326
Wesley Maurmair <maurmair@gmail.com>
2019-03-14 17:08:12 +0100

    Chapter8Done

---
+++ src/chapter8/Circle.java
@@ -0,0 +1,70 @@
+package chapter8;
+
+public class Circle {
+    public static final int ANGLES = 0;
+    private static int count = 0;
+    private int x;
+    private int y;
+    private int radius;
+
+    public Circle(){
+        this(0);
+    }
+    public Circle(final int radius){
+        this(Math.abs(radius),0,0);
+    }
+    public Circle(final int radius, final int x, final int y){
+        this.radius = Math.abs(radius);
+        this.x = Math.abs(x);
+        this.y = Math.abs(y);
+        count++;
+    }
+
+    public Circle(final Circle circle){
+        this.x = circle.getX();
+        this.y = circle.getY();
+        this.radius = Math.abs(circle.getRadius());
+        count++;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    public int getRadius() {
+        return Math.abs(radius);
+    }
+
+    public void setRadius(int radius) {
+        this.radius = Math.abs(radius);
+    }
+
+    public double getArea(final Circle cirlce){
+        return Math.abs(cirlce.getRadius()*Math.PI);
+    }
+
+    public double getPerimiter(final Circle cirle){
+        return Math.abs((cirle.radius*2)*Math.PI);
+    }
+
+    public static void grow(Circle circle){
+        circle.radius += 1;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+
+}
---
+++ src/chapter8/CircleApp.java
@@ -0,0 +1,33 @@
+package chapter8;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        System.out.println("This program uses a Circle");
+        Circle c1 = new Circle();
+        c1.setX(1);
+        c1.setY(2);
+        c1.setRadius(4);
+        printCInfo(c1,"C1");
+        Circle c2 = new Circle();
+        c2.setY(-1);
+        c2.setX(-2);
+        c2.setRadius(-5);
+        printCInfo(c2, "C2");
+        Circle c3 = new Circle(3);
+        printCInfo(c3, "C3");
+        Circle c4 = new Circle(6,1,4);
+        printCInfo(c4, "C4");
+        Circle c5 = new Circle(c4);
+        printCInfo(c5, "C5");
+    }
+
+    private static void printCInfo(final Circle c, final String name){
+        System.out.println("###### "+ name + " ######");
+        System.out.println("X is: " + c.getX());
+        System.out.println("Y is: " + c.getY());
+        System.out.println("Radius is: " + c.getRadius());
+        System.out.println("Oppervlakte is : " + c.getArea(c) + " vierkante meter");
+        System.out.println("Omtrekt is: " + c.getPerimiter(c)+ " meter");
+        System.out.println("Aantal gemaakte circles: "+c.getCount());
+    }
+}
